9ed846f951e7505e5494fa858d11db88ac9386e4
Solpad <rybakovartem03@gmail.com>
2022-10-25 21:03:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/graphic/MyDraw.kt
@@ -0,0 +1,37 @@
+package com.example.graphic
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.view.View
+
+class MyDraw(context:Context): View(context){
+
+    private val paint:Paint = Paint()
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        paint.apply {
+            style = Paint.Style.FILL
+            color = Color.WHITE
+        }
+        canvas?.drawPaint(paint)
+        paint.apply {
+            isAntiAlias = true
+            color = Color .YELLOW
+        }
+        canvas?.drawCircle(950F,30F,100F,paint)
+        paint.color = Color.GREEN
+        canvas?.drawRect(20F,650F,950F,680F,paint)
+
+        paint.apply {
+            color = Color.BLUE
+            style = Paint.Style.FILL
+            isAntiAlias = true
+            textSize=150F
+        }
+        canvas?.drawText("Only for cat",30F,648F,paint)
+    }
+}
